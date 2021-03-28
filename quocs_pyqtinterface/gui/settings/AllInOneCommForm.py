a26f40d8d315ea333934f0b79c9e85c971b62cd1
marcorossignolo <marcorossignolo92@gmail.com>
2021-03-28 17:13:09 +0200

    Implemented get results folder feature in AllInOne communication

--- quocs_pyqtinterface/gui/settings/AllInOneCommForm.py
+++ quocs_pyqtinterface/gui/settings/AllInOneCommForm.py
@@ -29,17 +29,26 @@ class AllInOneCommForm(QtWidgets.QWidget, Ui_Form):
         self.show()
         # dictionary
         self.all_in_one_communication_dictionary = AllInOneCommunicationDictionary(loaded_dictionary=loaded_dictionary)
-        # Button connection
+        # Connections
         self.get_results_folder_button.clicked.connect(self.get_results_folder)
+        self.results_folder_edit_line.textChanged.connect(self.results_folder_changed)
         # initialization
         self._initialization()
 
     def _initialization(self):
         self.results_folder_edit_line.setText(self.all_in_one_communication_dictionary.results_folder)
 
+    def results_folder_changed(self, results_folder_path):
+        if os.path.isdir(results_folder_path):
+            self.all_in_one_communication_dictionary.results_folder = results_folder_path
+        else:
+            self.all_in_one_communication_dictionary.results_folder = os.path.join(os.getcwd(), "QuOCS_Results")
+
     def get_results_folder(self):
-        # TODO get the results folder
-        pass
+        # Get the results folder form a file
+        folder = QtWidgets.QFileDialog.getExistingDirectory(self, "Get results directory", os.getcwd(),
+                                                            options=QtWidgets.QFileDialog.DontUseNativeDialog)
+        self.results_folder_edit_line.setText(folder)
 
     def get_dictionary(self):
         return self.all_in_one_communication_dictionary.get_dictionary()

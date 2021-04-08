7926e2d21ff4041ba90ba0ab9b362ba8e759fc0e
marcorossignolo <marcorossignolo92@gmail.com>
2021-04-08 13:34:48 +0200

    Update the conversion script from ui to py with pyside2 command

--- quocs_pyqtinterface/gui/convertuipy.py
+++ quocs_pyqtinterface/gui/convertuipy.py
@@ -21,5 +21,5 @@ class_folder = os.path.join(os.getcwd(), "uiclasses")
 for file in os.scandir(ui_folder):
     if file.path.endswith(".ui"):
         class_file = os.path.splitext(file)[0] + "UI.py"
-        os.system("python -m PyQt5.uic.pyuic -x {0} -o {1}".format(file.path, class_file))
+        os.system("pyside2-uic {0} -o {1}".format(file.path, class_file))
         os.system("mv {0} {1}".format(class_file, class_folder))

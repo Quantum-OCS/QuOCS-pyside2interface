8a84573a7a8519f849aba6628768130f9211016a
thomas <t.reisser@fz-juelich.de>
2022-02-02 10:19:19 +0100

    Made conversion differentiate between Windows and other OS

--- src/quocspyside2interface/gui/convertuipy_pyqt5.py
+++ src/quocspyside2interface/gui/convertuipy_pyqt5.py
@@ -14,12 +14,16 @@
 #  limitations under the License.
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-import os
+import os, platform
 
 ui_folder = os.path.join(os.getcwd(), "uifiles")
 class_folder = os.path.join(os.getcwd(), "uiclasses")
+operating_sys = platform.system()
 for file in os.scandir(ui_folder):
     if file.path.endswith(".ui"):
         class_file = os.path.splitext(file)[0] + "UI.py"
         os.system("pyuic5 -x {0} -o {1}".format(file.path, class_file))
-        os.system("mv {0} {1}".format(class_file, class_folder))
+        if operating_sys == 'Windows':
+            os.system("move {0} {1}".format(class_file, class_folder))
+        else:
+            os.system("mv {0} {1}".format(class_file, class_folder))
